99c885b9c32f06a441b0ce7fc4b26d71beedbefb
JustHo <zalop3128@kakao.com>
2024-01-03 05:23:51 +0900

    orderBy 사용법

--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -161,14 +161,42 @@ public class QuerydslBasicTest {
                 .fetchCount();
 
         /*
-        * fetch() : 리스트 조회, 데이터 없으면 빈 리스트 반환
-        * fetchOne() : 단 건 조회
-        * -> 결과가 없다면 Null
-        * -> 결과가 둘 이상이면 'com.querydsl.core.NonUniqueResultException' 에러 발생
-        * fetchFirst() : 'limit(1).fetchOne()'
-        * fetchResults() : 페이징 정보 포함, total count 쿼리 추가 실행
-        * fetchCount() : count 쿼리로 변경해서 count 수 조회
-        * */
+         * fetch() : 리스트 조회, 데이터 없으면 빈 리스트 반환
+         * fetchOne() : 단 건 조회
+         * -> 결과가 없다면 Null
+         * -> 결과가 둘 이상이면 'com.querydsl.core.NonUniqueResultException' 에러 발생
+         * fetchFirst() : 'limit(1).fetchOne()'
+         * fetchResults() : 페이징 정보 포함, total count 쿼리 추가 실행
+         * fetchCount() : count 쿼리로 변경해서 count 수 조회
+         * */
+    }
+
+
+    /*
+     * 회원 정렬 순서
+     * 1. 회원 나이 내림차순(desc)
+     * 2. 회원 이름 올림차순(asc)
+     * 단 2에서 회원 이름이 엇으면 마지막에 출력(nulls last)
+     * */
+    @Test
+    public void sort() {
+        qFactory = new JPAQueryFactory(em);
+        em.persist(new Member(null, 100));
+        em.persist(new Member("member5", 100));
+        em.persist(new Member("member6", 100));
+        List<Member> result = qFactory
+                .selectFrom(member)
+                .where(member.age.eq(100))
+                .orderBy(member.age.desc(), member.username.asc().nullsLast())
+                .fetch();
+
+        Member member5 =result.get(0);
+        Member member6 =result.get(1);
+        Member memberNull = result.get(2);
+        assertThat(member5.getUsername()).isEqualTo("member5");
+        assertThat(member6.getUsername()).isEqualTo("member6");
+        assertThat(memberNull.getUsername()).isNull();
+
     }
 
 
