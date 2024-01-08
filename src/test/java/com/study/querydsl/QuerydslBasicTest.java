1eefddddc6aecf5353987d1409e49baf5b478b86
JustHo <zalop3128@kakao.com>
2024-01-09 05:57:00 +0900

    동적쿼리 - BooleanBuilder 사용

--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -1,6 +1,7 @@
 package com.study.querydsl;
 
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ExpressionUtils;
@@ -655,11 +656,37 @@ public class QuerydslBasicTest {
                 .from(member)
                 .fetch();
 
-        for(MemberDto memberDto : result){
+        for (MemberDto memberDto : result) {
             System.out.println("memberDto : " + memberDto);
         }
     }
     // @QueryProjection의 단점은 DTO가 annotation에 의존해야한다.
+
+    @Test
+    public void dynamicQuery_BooleanBuilder() {
+        String usernameParam = "member1";
+        Integer ageParam = 10;
+
+        List<Member> result = searchMember1(usernameParam, ageParam);
+        assertThat(result.size()).isEqualTo(1);
+
+    }
+
+    private List<Member> searchMember1(String usernameCond, Integer ageCond) {
+
+        BooleanBuilder builder = new BooleanBuilder();
+        if (usernameCond != null) {
+            builder.and(member.username.eq(usernameCond));
+        }
+        if (ageCond != null) {
+            builder.and(member.age.eq(ageCond));
+        }
+
+        return qFactory
+                .selectFrom(member)
+                .where(builder)
+                .fetch();
+    }
 
 
 }
